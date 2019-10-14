ee94402d4ff826d2a7a6398dc213d5ded6e74e74
Ravi Prakash Pandey <ravi.pandey13@wipro.com>
2019-10-14 19:13:04 +0530

    added some features

--- routes/verify.js
+++ routes/verify.js
@@ -1,16 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const verify = (req,res,next) => {
-    const token = req.header('authToken');
-    if (!token) return res.status(401).send('Access Denied. Please log in.');
+const verify = (req, res, next) => {
+  const token = req.header("authToken");
+  if (!token) return res.status(401).send("Access Denied. Please log in.");
 
-    try {
-        const verified = jwt.verify(token, process.env.SECRET);
-        req.user = verified;
-        next();
-    } catch(err) {
-        return res.status(400).send('Invalid token.');
-   }
-}
+  try {
+    const verified = jwt.verify(
+      token,
+      process.env.SECRET ||
+        "bGVgGfug766gg&VFtvty%sdgfhdrttrfr7676vtg5wf321hj9056ccb3f"
+    );
+    req.user = verified;
+    next();
+  } catch (err) {
+    return res.status(400).send("Invalid token.");
+  }
+};
 
 module.exports = verify;
